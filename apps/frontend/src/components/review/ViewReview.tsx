a6539a8216ea496d178cee44c2dd6c4dbf3dda66
lerxcl <lerx98@gmail.com>
2023-04-05 15:44:02 +0800

    Abstract component ViewImages for ReviewList and ViewReview

--- apps/frontend/src/components/review/ViewReview.tsx
+++ apps/frontend/src/components/review/ViewReview.tsx
@@ -16,22 +16,18 @@ import {
   HStack,
   Flex,
   Spacer,
-  IconButton,
-  useBreakpointValue,
 } from '@chakra-ui/react';
-import React from 'react';
-import { Key, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import ReactStars from 'react-rating-stars-component';
 import { useNavigate } from 'react-router-dom';
-import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
-import Slider from 'react-slick';
 
 import { getCafeDetail } from '../../services/api_service';
 import Sentiment from '../authenticity-senti/Sentiment';
 import Authenticity from '../authenticity-senti/Authenticity';
 import { ReviewModel } from 'apps/backend/src/review/review.interface';
+import { ViewImages } from './ViewImages';
 
 export function ViewReview({ review }: { review: ReviewModel }) {
   const [cafeLogo, setCafeLogo] = useState('');
@@ -47,26 +43,6 @@ export function ViewReview({ review }: { review: ReviewModel }) {
     setCafeDetails(cafe_details);
     setCafeLogo(cafe_details.logo);
   };
-
-  // Settings for the slider
-  const settings = {
-    dots: true,
-    arrows: false,
-    fade: true,
-    infinite: true,
-    autoplay: true,
-    speed: 500,
-    autoplaySpeed: 5000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-  // As we have used custom buttons, we need a reference variable to
-  // change the state
-  const [slider, setSlider] = React.useState<Slider | null>(null);
-  // These are the breakpoints which changes the position of the
-  // buttons as the screen size changes
-  const top = useBreakpointValue({ base: '90%', md: '50%' });
-  const side = useBreakpointValue({ base: '30%', md: '10px' });
 
   return (
     <Card>
@@ -463,65 +439,7 @@ export function ViewReview({ review }: { review: ReviewModel }) {
             </Text>
           </Box>
 
-          <Box position={'relative'} width={'full'} overflow={'hidden'}>
-            {/* CSS files for react-slick */}
-            <link
-              rel="stylesheet"
-              type="text/css"
-              charSet="UTF-8"
-              href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-            />
-            <link
-              rel="stylesheet"
-              type="text/css"
-              href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-            />
-            {/* Left Icon */}
-            <IconButton
-              aria-label="left-arrow"
-              colorScheme="messenger"
-              borderRadius="full"
-              position="absolute"
-              left={side}
-              top={top}
-              transform={'translate(0%, -50%)'}
-              zIndex={2}
-              onClick={() => slider?.slickPrev()}
-            >
-              <BiLeftArrowAlt />
-            </IconButton>
-            {/* Right Icon */}
-            <IconButton
-              aria-label="right-arrow"
-              colorScheme="messenger"
-              borderRadius="full"
-              position="absolute"
-              right={side}
-              top={top}
-              transform={'translate(0%, -50%)'}
-              zIndex={2}
-              onClick={() => slider?.slickNext()}
-            >
-              <BiRightArrowAlt />
-            </IconButton>
-            {/* Slider */}
-            <Slider {...settings} ref={(slider: any) => setSlider(slider)}>
-              {review.image_url !== undefined &&
-                review.image_url.map(
-                  (url: any, index: Key | null | undefined) => (
-                    <Box
-                      key={index}
-                      height={'6xl'}
-                      position="relative"
-                      backgroundPosition="center"
-                      backgroundRepeat="no-repeat"
-                      backgroundSize="cover"
-                      backgroundImage={`url(${url})`}
-                    />
-                  )
-                )}
-            </Slider>
-          </Box>
+          <ViewImages images={review.image_url}></ViewImages>
         </Stack>
       </CardBody>
     </Card>
