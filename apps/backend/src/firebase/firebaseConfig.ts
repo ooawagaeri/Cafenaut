d8577e6c5e235716961c6511091f62bf8f5d629c
ooawagaeri <oastreach@gmail.com>
2023-04-13 21:00:37 +0800

    chore: remove firebase config

--- apps/backend/src/firebase/firebaseConfig.ts
+++
@@ -1,9 +0,0 @@
-export const config = {
-    "apiKey": "AIzaSyC58tOLUBFaRVvWqQo4J-tnfI_GD5I3bAQ",
-    "authDomain": "cafenaut-511b2.firebaseapp.com",
-    "projectId": "cafenaut-511b2",
-    "storageBucket": "cafenaut-511b2.appspot.com",
-    "messagingSenderId": "1008137617927",
-    "appId": "1:1008137617927:web:f6b296f68df3de7835e0a2",
-    "measurementId": "G-KXQ8Q36XVZ"
-}
